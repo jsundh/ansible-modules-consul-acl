a49e0daa83c34b501557e3a9bbc69cb9486d63ca
Jakob Sundh <jsundh@users.noreply.github.com>
2019-02-17 19:55:09 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+long_description = open("README.md", "r").read().strip()
+
+setuptools.setup(
+    name="ansible-modules-consul-acl",
+    version="0.0.1",
+    description="Ansible modules for the Consul ACL system",
+    long_description=long_description,
+    author="Jakob Sundh",
+    author_email="jsundh@users.noreply.github.com",
+    url="https://github.com/jsundh/ansible-modules-consul-acl",
+    packages=["ansible/modules/consul_acl"],
+    install_requires=["ansible>=2.4.0"],
+    license="GNU GPL v3",
+)
